e7dd3f3d269e75a511e6f92c96550f0efd1ff213
Grant Rettke <grant@wisdomandwonder.com>
2021-12-21 09:07:06 -0600

    Skeleton renders two objects one clone.

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # inkbxm
-INKscape BoXMaster Extension creates box clones.
+
+INKscape BoXMaster Extension
+
+# Coding
+
+```bash
+rm -rf inkbxm
+git clone git@github.com:grettke/inkbxm.git
+cd -
+````
+
+# Deployment
+
+## macOS
+
+```bash
+cd /Applications/Inkscape.app/Contents/Resources/share/inkscape/extensions
+rm inkbxm.inx
+rm inkbxm.py
+ln -s ~/src/inkbxm/inkbxm.inx inkbxm.inx
+ln -s ~/src/inkbxm/inkbxm.py inkbxm.py
+cd -
+```
---
+++ inkbxm.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding=utf-8
+#
+# Copyright (C) 2021 Grant Rettke <grant@wisdomandwonder.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+import inkex
+
+
+class BoxMaster(inkex.EffectExtension):
+
+    def add_arguments(self, pars):
+        pars.add_argument("--rows", type=int,
+                          default=1, help="Number of rows")
+        pars.add_argument("--columns", type=int,
+                          default=1, help="Number of columns")
+
+    def effect(self):
+        """
+        This cloning approach follows Nup.generate_nup.make_clones in
+        extensions/layout_nup.py.
+        """
+        layer = self.svg.add(inkex.Layer.new('bxmlayer'))
+        source = inkex.Rectangle(x='0', y='0', width='10', height='10')
+        model = layer.add(source.copy())
+        model.set('id', 'bxmmodel')
+        use = layer.add(inkex.Use())
+        use.set('xlink:href', '#' + 'bxmmodel')
+        use.transform.add_translate(20, 20)
+
+
+if __name__ == '__main__':
+    BoxMaster().run()
